bcd61b44b36422c55b1823e97b69683aafceca37
Ferhan Akkan <>
2020-12-01 21:54:51 +0300

    Code refactor

--- Speedometer/Shared/AppManager.swift
+++ Speedometer/Shared/AppManager.swift
@@ -23,14 +23,12 @@ class AppManager {
 extension AppManager {
     
     
-    
-    
     func appLaunchLanguage() {
-        if let selectedLanguage = UserDefaults.standard.string(forKey: "lang") {
-            let path = Bundle.main.path(forResource: selectedLanguage, ofType: "lproj")
+        if let selectedLanguage = UserDefaults.standard.string(forKey: Constants.Language.langKey) {
+            let path = Bundle.main.path(forResource: selectedLanguage, ofType: Constants.Language.langpath)
             self.bundle = Bundle(path: path!)!
         } else {
-            let supportedLang = ["en","tr"]
+            let supportedLang = [Language.english.rawValue,Language.turkish.rawValue]
             let currentDeviceLang = Locale.current.languageCode
             if supportedLang.contains(currentDeviceLang!) {
                 setLanguage(language: currentDeviceLang!)
@@ -41,21 +39,21 @@ extension AppManager {
     }
     
     func setLanguage(language: Language) {
-        UserDefaults.standard.setValue(language.rawValue, forKey: "lang")
-        let path = Bundle.main.path(forResource: language.rawValue, ofType: "lproj")
+        UserDefaults.standard.setValue(language.rawValue, forKey: Constants.Language.langKey)
+        let path = Bundle.main.path(forResource: language.rawValue, ofType: Constants.Language.langpath)
         self.bundle = Bundle(path: path!)!
 //        let _ = RootSelector()
     }
     
     func setLanguage(language: String) {
-        UserDefaults.standard.setValue(language, forKey: "lang")
-        let path = Bundle.main.path(forResource: language, ofType: "lproj")
+        UserDefaults.standard.setValue(language, forKey: Constants.Language.langKey)
+        let path = Bundle.main.path(forResource: language, ofType: Constants.Language.langpath)
         self.bundle = Bundle(path: path!)!
 //        let _ = RootSelector()
     }
     
     func getLanguage() -> String {
-        return UserDefaults.standard.string(forKey: "lang")!
+        return UserDefaults.standard.string(forKey: Constants.Language.langKey)!
     }
     
 }
@@ -65,7 +63,7 @@ extension AppManager {
 
 func messagePresent(title: String, message: String, moreButtonAction: [UIAlertAction]?) {
     let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
-    alert.addAction(UIAlertAction(title: "Tamam", style: .default, handler: { (_) in
+    alert.addAction(UIAlertAction(title: Constants.AlertMessage.ok, style: .default, handler: { (_) in
         
     }))
     
--- Speedometer/Supporting Files/Constants/Constants.swift
+++ Speedometer/Supporting Files/Constants/Constants.swift
@@ -10,11 +10,18 @@ import Foundation
 struct Constants {
     
      struct Language {
-        static var english: String = "en"
-        static var turkish: String = "tr"
+        static var langKey = "lang"
+        static var langpath = "lproj"
     }
     
     struct Badge {
         static let badgeNumber = "badge"
     }
+    
+    struct AlertMessage {
+        static var title = "alertTitle".localized()
+        static var ok = "alertOkButton".localized()
+    }
 }
+
+
